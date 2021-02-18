40144cdf9cc048c3080d3bfc4cd4a9f79bb47a49
fakihfavian24 <fakihfavianwibowo@gmail.com>
2021-02-18 16:05:04 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Fakih Favian Wibowo
+203040073
+https://github.com/fakihfavian24/pw2021_203040073
+Pertemuan 2 - 18 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Fakih Favian Wibowo";
+// echo 'Halo, nama saya $nama';
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Fakih Favian";
+// $nama_belakang = "Wibowo";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -=5;
+// echo $x;
+// $nama = "Fakih Favian";
+// $nama .= " ";
+// $nama .= "Wibowo";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1")
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+?>
