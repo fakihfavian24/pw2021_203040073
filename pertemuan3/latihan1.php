45f75c28a6445c9853a155de964affded877b389
fakihfavian24 <fakihfavianwibowo@gmail.com>
2021-02-19 15:43:12 +0700

    mengerjakan video pertemuan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Fakih Favian Wibowo
+203040073
+https://github.com/fakihfavian24/pw2021_203040073
+Pertemuan 3 - 19 Februari 2021
+Mempelajari mengenai Struktur Kendali
+*/
+?>
+<?php
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++ ) {
+//     echo "Hello World! <br>";
+// }
+?>
