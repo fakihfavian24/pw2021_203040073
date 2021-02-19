2297e10cb6bf3515534a8aab189fd4cd25709632
fakihfavian24 <fakihfavianwibowo@gmail.com>
2021-02-19 19:44:24 +0700

    mengerjakan video pertemuan 3

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -17,4 +17,14 @@ Mempelajari mengenai Struktur Kendali
 // for( $i = 0; $i < 5; $i++ ) {
 //     echo "Hello World! <br>";
 // }
+// $i = 10;
+// while( $i < 5 ) {
+//     echo "Hello World! <br>";
+// $i++;
+// }
+// $i = 10;
+// do {
+//     echo "Hello World! <br>";
+//     $i++;
+// } while( $i < 5 );
 ?>
