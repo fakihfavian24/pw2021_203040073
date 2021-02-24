09fc7f23e81fdf724c725736ee8b741569ef4873
fakihfavian24 <fakihfavianwibowo@gmail.com>
2021-02-24 15:49:33 +0700

    Mengerjakan video pertemjuan 3

--- pertemuan3/latihan1a.php
+++ pertemuan3/latihan1a.php
@@ -3,17 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Latihan 1</title>
+    <style>
+    .warna-baris {
+        background-color: silver;
+    }
+    </style>
 </head>
 <body>
     
     <table border="1" cellpadding="10" cellspacing="0">
-    <?php for( $i = 1; $i <= 3; $i++ ) { ?>
-        <tr>
-            <?php for( $j = 1; $j <= 5; $j++ ) { ?>
-                <td><?php echo "$i, $j"; ?></td>
-            <?php } ?>
+    <?php for( $i = 1; $i <= 5; $i++ ) : ?>
+    <?php if( $i % 2 == 0 ) : ?>
+        <tr class="warna-baris">
+        <?php else : ?>
+            <tr>
+        <?php endif; ?>
+            <?php for( $j = 1; $j <= 5; $j++ ) : ?>
+                <td><?= "$i, $j"; ?></td>
+            <?php endfor; ?>
         </tr>
-    <?php } ?>
+    <?php endfor; ?>
     </table>
 
 </body>
