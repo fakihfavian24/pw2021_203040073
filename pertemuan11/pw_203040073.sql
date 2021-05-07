7313c6e7565af39f08f2e3fbd127e0295c64796d
fakihfavian24 <fakihfavianwibowo@gmail.com>
2021-05-07 09:53:27 +0700

    menambahkan fungsi hapus, ubah dan pencarian

---
+++ pertemuan11/pw_203040073.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Apr 2021 pada 15.38
+-- Versi server: 10.4.16-MariaDB
+-- Versi PHP: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw_203040073`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `nrp` char(9) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALUES
+(1, 'Iron Man', '203040070', 'ironman@gmail.com', 'Teknik Mesin', '1.jpg'),
+(2, 'Hulk', '203040071', 'hulk@gmail.com', 'Teknik Industri', '2.jpg'),
+(3, 'Thanos', '203040072', 'thanos@gmail.com', 'Teknik Sipil', '3.jpg'),
+(4, 'Black Widow', '203040073', 'blackwidow@gmail.com', 'Teknik Lingkungan', '4.jpg'),
+(5, 'Captain Amerika', '203040074', 'captainamerika@gmail.com', 'Teknik Informatika', '5.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- praktikum/tugas6/latihan6c/index.php
+++ praktikum/tugas6/latihan6c/index.php
@@ -13,7 +13,7 @@ require 'php/functions.php';
 // melakukan query
 $laptop = query("SELECT * FROM laptop");
 
-if( isset($_POST["cari"]) ) {
+if (isset($_POST["cari"])) {
     $laptop = cari($_POST["keyword"]);
 }
 
@@ -21,82 +21,85 @@ if( isset($_POST["cari"]) ) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- Compiled and minified CSS -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styleadmin4.css">
+    <link rel="stylesheet" href="css/styleindex.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <title>Daftar Laptop</title>
 </head>
+
 <body>
 
     <div class="navbar-fixed">
-    <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">CENTRALstore</a>
-    </div>
-  </nav>
-  <br>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">CENTRALstore</a>
+            </div>
+        </nav>
+        <br>
 
-    <h3>Daftar Harga Laptop 2021</h3>
-    <br>
+        <h3>Daftar Harga Laptop 2021</h3>
+        <br>
 
-    <div class="admin">
-        <a href="php/login.php" class="tbl-grey">Masuk ke halaman Admin</a>
-    </div>
-    <br>
+        <div class="admin">
+            <a href="php/login.php" class="tbl-grey">Masuk ke halaman Admin</a>
+        </div>
+        <br>
 
-<div class="input">
-<form action="" method="post">
+        <div class="input">
+            <form action="" method="post">
 
-    <input type="text" name="keyword" autofocus placeholder="masukkan keyword pencarian.." autocomplete="off">
-    <button type="submit" name="cari"><i class="Tiny material-icons">search</i></button>
+                <input type="text" name="keyword" autofocus placeholder="masukkan keyword pencarian.." autocomplete="off">
+                <button type="submit" name="cari"><i class="Tiny material-icons">search</i></button>
 
-</form>
-</div>
-<br>
+            </form>
+        </div>
+        <br>
 
-<div class="font">
-<div class="container mt-3 mb-3 text-center">
-        <table class="table table-bordered table-hover highlight - centered white">
-            <thead>
-                <tr class="light-blue darken-3">
-                    <th scope="col">No</th>
-                    <th scope="col">Picture</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Detail</th>
-                </tr>
-            </thead>
-        <tbody>
-        <?php if (empty($laptop)) : ?>
-            <tr>
-                <td colspan="7">
-                    <h1>Data tidak ditemukan</h1>
-                </td>
-            </tr>
-        <?php else : ?>
-            <?php foreach($laptop as $lp => $l) : ?>
-            <tr>
-                <td><?= $lp+1 ?></td>
-                <td>
-                    <img src="assets/img/<?= $l["picture"]; ?>">
-                </td>
-                <td><?= $l["name"]; ?></td>
-                <td>
-                    <p><a href="php/detail.php?id=<?= $l['id'] ?>" class="tbl-biru" <?= $l["name"] ?>>Detail</a></p>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-            <?php endif; ?>
-        </tbody>
-    </div>
-    </div>
+        <div class="font">
+            <div class="container mt-3 mb-3 text-center">
+                <table class="table table-bordered table-hover highlight - centered white">
+                    <thead>
+                        <tr class="light-blue darken-3">
+                            <th scope="col">No</th>
+                            <th scope="col">Picture</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Detail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if (empty($laptop)) : ?>
+                            <tr>
+                                <td colspan="7">
+                                    <h1>Data tidak ditemukan</h1>
+                                </td>
+                            </tr>
+                        <?php else : ?>
+                            <?php foreach ($laptop as $lp => $l) : ?>
+                                <tr>
+                                    <td><?= $lp + 1 ?></td>
+                                    <td>
+                                        <img src="assets/img/<?= $l["picture"]; ?>">
+                                    </td>
+                                    <td><?= $l["name"]; ?></td>
+                                    <td>
+                                        <p><a href="php/detail.php?id=<?= $l['id'] ?>" class="tbl-biru" <?= $l["name"] ?>>Detail</a></p>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </tbody>
+            </div>
+        </div>
 </body>
 </table>
 <br>
+
 </html>
